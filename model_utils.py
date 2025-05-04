45e2a5c9ea0cca3e2630b16e3257b6c046bf93eb
Shiva Sai Chakradhar <152002187+SHIVASAICHAKRADHAR@users.noreply.github.com>
2025-05-04 21:36:31 +0530

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -22,5 +22,28 @@ def load_text_resources():
         tokenizer = pickle.load(f)
     with open('label_encoder_text.pkl', 'rb') as f:
         label_enc = pickle.load(f)
-    return text_model, tokenizer, label_enc
+    return text_model, tokenizer, label_encimport pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+
+def load_audio_resources():
+    """Load audio model and encoders"""
+    audio_model = load_model('full_audio_metadata_model.h5')
     
+    # Load all encoders
+    encoders = {}
+    for enc_name in ['actor', 'emotion', 'intensity', 'modality', 
+                    'repetition', 'statement', 'vocal']:
+        with open(f'{enc_name}_encoder.pkl', 'rb') as f:
+            encoders[enc_name] = pickle.load(f)
+    
+    return audio_model, encoders
+
+def load_text_resources():
+    """Load text model resources"""
+    text_model = load_model('text_model.h5')
+    with open('tokenizer.pkl', 'rb') as f:
+        tokenizer = pickle.load(f)
+    with open('label_encoder_text.pkl', 'rb') as f:
+        label_enc = pickle.load(f)
+    return text_model, tokenizer, label_enc
